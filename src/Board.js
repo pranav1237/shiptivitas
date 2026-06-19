3eeac931669bb1f5928f8c4d7f6b100d3876dd46
Pranav Mahajan <pranavmahajan.4122005@gmail.com>
2026-06-19 20:15:12 +0530

    Update Board.js

--- src/Board.js
+++ src/Board.js
@@ -7,52 +7,101 @@ import './Board.css';
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
-    const clients = this.getClients();
+
+    const clients = this.getClients().map(client => ({
+      ...client,
+      status: 'backlog'
+    }));
+
     this.state = {
       clients: {
-        backlog: clients.filter(client => !client.status || client.status === 'backlog'),
-        inProgress: clients.filter(client => client.status && client.status === 'in-progress'),
-        complete: clients.filter(client => client.status && client.status === 'complete'),
+        backlog: clients,
+        inProgress: [],
+        complete: []
       }
-    }
+    };
+
     this.swimlanes = {
       backlog: React.createRef(),
       inProgress: React.createRef(),
-      complete: React.createRef(),
+      complete: React.createRef()
+    };
+  }
+
+  componentDidMount() {
+    this.drake = Dragula([
+      this.swimlanes.backlog.current,
+      this.swimlanes.inProgress.current,
+      this.swimlanes.complete.current
+    ]);
+
+    this.drake.on('drop', (el, target) => {
+      el.classList.remove(
+        'Card-grey',
+        'Card-blue',
+        'Card-green'
+      );
+
+      if (target === this.swimlanes.backlog.current) {
+        el.classList.add('Card-grey');
+        el.setAttribute('data-status', 'backlog');
+      }
+
+      if (target === this.swimlanes.inProgress.current) {
+        el.classList.add('Card-blue');
+        el.setAttribute('data-status', 'in-progress');
+      }
+
+      if (target === this.swimlanes.complete.current) {
+        el.classList.add('Card-green');
+        el.setAttribute('data-status', 'complete');
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.drake) {
+      this.drake.destroy();
     }
   }
+
   getClients() {
     return [
-      ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
-      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation', 'complete'],
-      ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-      ['4','Thompson PLC','Streamlined Regional Knowledgeuser', 'in-progress'],
-      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix', 'in-progress'],
-      ['6','Boehm and Sons','Automated Systematic Paradigm', 'backlog'],
-      ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy', 'backlog'],
-      ['8','Schumm-Labadie','Operative Heuristic Challenge', 'backlog'],
-      ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-      ['10','Romaguera Inc','Managed Foreground Toolset', 'backlog'],
-      ['11','Reilly-King','Future-Proofed Interactive Toolset', 'complete'],
-      ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability', 'backlog'],
-      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website', 'complete'],
-      ['14','Borer LLC','Profit-Focused Incremental Orchestration', 'backlog'],
-      ['15','Emmerich-Ankunding','User-Centric Stable Extranet', 'in-progress'],
-      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access', 'in-progress'],
-      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty', 'complete'],
-      ['18','Bins, Toy and Klocko','Integrated Assymetric Software', 'backlog'],
-      ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline', 'backlog'],
-      ['20','Murphy, Lang and Ferry','Organized Explicit Access', 'backlog'],
+      ['1','Stark, White and Abbott','Cloned Optimal Architecture'],
+      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation'],
+      ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface'],
+      ['4','Thompson PLC','Streamlined Regional Knowledgeuser'],
+      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix'],
+      ['6','Boehm and Sons','Automated Systematic Paradigm'],
+      ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy'],
+      ['8','Schumm-Labadie','Operative Heuristic Challenge'],
+      ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude'],
+      ['10','Romaguera Inc','Managed Foreground Toolset'],
+      ['11','Reilly-King','Future-Proofed Interactive Toolset'],
+      ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability'],
+      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website'],
+      ['14','Borer LLC','Profit-Focused Incremental Orchestration'],
+      ['15','Emmerich-Ankunding','User-Centric Stable Extranet'],
+      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access'],
+      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty'],
+      ['18','Bins, Toy and Klocko','Integrated Assymetric Software'],
+      ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline'],
+      ['20','Murphy, Lang and Ferry','Organized Explicit Access']
     ].map(companyDetails => ({
       id: companyDetails[0],
       name: companyDetails[1],
       description: companyDetails[2],
-      status: companyDetails[3],
+      status: 'backlog'
     }));
   }
+
   renderSwimlane(name, clients, ref) {
     return (
-      <Swimlane name={name} clients={clients} dragulaRef={ref}/>
+      <Swimlane
+        name={name}
+        clients={clients}
+        dragulaRef={ref}
+      />
     );
   }
 
@@ -62,13 +111,27 @@ export default class Board extends React.Component {
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-4">
-              {this.renderSwimlane('Backlog', this.state.clients.backlog, this.swimlanes.backlog)}
+              {this.renderSwimlane(
+                'Backlog',
+                this.state.clients.backlog,
+                this.swimlanes.backlog
+              )}
             </div>
+
             <div className="col-md-4">
-              {this.renderSwimlane('In Progress', this.state.clients.inProgress, this.swimlanes.inProgress)}
+              {this.renderSwimlane(
+                'In Progress',
+                this.state.clients.inProgress,
+                this.swimlanes.inProgress
+              )}
             </div>
+
             <div className="col-md-4">
-              {this.renderSwimlane('Complete', this.state.clients.complete, this.swimlanes.complete)}
+              {this.renderSwimlane(
+                'Complete',
+                this.state.clients.complete,
+                this.swimlanes.complete
+              )}
             </div>
           </div>
         </div>
